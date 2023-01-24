e3d42307e4dc96d9ddb25eb351fb42d0d2fd50ad
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-01-23 23:44:55 -0800

    some progress

--- src/providers/pathfinding/PathfindingQuadTree.ts
+++ src/providers/pathfinding/PathfindingQuadTree.ts
@@ -1,40 +1,123 @@
+import { FromGridX, FromGridY, ToGridX, ToGridY } from "@rpg-engine/shared";
 import { Rectangle } from "@timohausmann/quadtree-ts";
 import { provide } from "inversify-binding-decorators";
 import PF from "pathfinding";
 import { MapSolidsQuadTree } from "./MapSolidsQuadTree";
+
+interface IConvertedCoordinates {
+  startGridX: number;
+  startGridY: number;
+  endGridX: number;
+  endGridY: number;
+}
+
 @provide(PathfindingQuadTree)
 export class PathfindingQuadTree {
   constructor(private mapSolidsQuadTree: MapSolidsQuadTree) {}
 
   public generateGridBetweenPoints(
+    map: string,
     startGridX: number,
     startGridY: number,
     endGridX: number,
-    endGridY: number,
-    quadTreeSolids: Rectangle[]
+    endGridY: number
   ): PF.Grid {
-    const matrix = [];
+    const solids = this.mapSolidsQuadTree.getSolidsInArea(
+      map,
+      new Rectangle({
+        x: FromGridX(startGridX),
+        y: FromGridY(startGridY),
+        width: FromGridX(endGridX - startGridX),
+        height: FromGridY(endGridY - startGridY),
+      })
+    );
 
-    // const area = this.mapSolidsQuadTree.getSolidsInArea(
-    //   map,
-    //   new Rectangle({
-    //     x: startGridX,
-    //   })
-    // );
-
-    for (let i = startGridX; i < endGridX; i++) {
-      matrix[i] = [];
-      for (let j = startGridY; j < endGridY; j++) {
-        const hasSolid = quadTreeSolids.some((solid) => {
-          return solid.x === i && solid.y === j;
-        });
-
-        matrix[i][j] = hasSolid ? 1 : 0;
-      }
-    }
+    const matrix = this.generateMatrixBetweenPoints(startGridX, startGridY, endGridX, endGridY, (gridX, gridY) =>
+      solids.some((solid) => {
+        return ToGridX(solid.x) === gridX && ToGridY(solid.y) === gridY;
+      })
+    );
 
     const pf = new PF.Grid(matrix);
 
     return pf;
+  }
+
+  public getNodeAt(grid: PF.Grid, gridX: number, gridY: number): PF.Node {
+    return grid.nodes[gridY][gridX];
+  }
+
+  public;
+  findShortestPathBetweenPoints(
+    map: string,
+    startGridX: number,
+    startGridY: number,
+    endGridX: number,
+    endGridY: number,
+    gridOffsetX: number,
+    gridOffsetY: number
+  ): PF.Node[] {
+    const grid = this.generateGridBetweenPoints(map, startGridX, startGridY, endGridX, endGridY);
+
+    const finder = new PF.AStarFinder();
+
+    const path = finder.findPath(0, 0, grid.width - 1, grid.height - 1, grid);
+
+    return path;
+  }
+
+  private generateMatrixBetweenPoints(
+    startGridX: number,
+    startGridY: number,
+    endGridX: number,
+    endGridY: number,
+    isSolidFn: (gridX: number, gridY: number) => boolean
+  ): number[][] {
+    const matrix: number[][] = [];
+
+    // generate width and height considering offset
+    const [width, height] = this.getWidthHeightBetweenPoints(startGridX, startGridY, endGridX, endGridY);
+
+    for (let gridY = startGridY; gridY < startGridY + height; gridY++) {
+      matrix[gridY - startGridY] = [];
+      for (let gridX = startGridX; gridX < startGridX + width; gridX++) {
+        const isWalkable = !isSolidFn(gridX, gridY);
+        matrix[gridY - startGridY][gridX - startGridX] = isWalkable ? 0 : 1;
+      }
+    }
+
+    if (!matrix.length) {
+      throw new Error("Failed to generate pathfinding grid");
+    }
+
+    return matrix;
+  }
+
+  private getWidthHeightBetweenPoints(
+    startGridX: number,
+    startGridY: number,
+    endGridX: number,
+    endGridY: number
+  ): [number, number] {
+    const width = Math.abs(endGridX - startGridX) + 1;
+    const height = Math.abs(endGridY - startGridY) + 1;
+
+    return [width, height];
+  }
+
+  convertCoordinates(
+    rawStartX: number,
+    rawStartY: number,
+    rawEndX: number,
+    rawEndY: number,
+    startX: number,
+    startY: number
+  ): IConvertedCoordinates {
+    return {
+      startGridX: rawStartX + startX,
+      startGridY: rawStartY + startY,
+      endGridX: rawEndX + startX,
+      endGridY: rawEndY + startY,
+    };
   }
 }
--- src/providers/pathfinding/__tests__/PathfindingQuadtree.spec.ts
+++ src/providers/pathfinding/__tests__/PathfindingQuadtree.spec.ts
@@ -1,41 +1,54 @@
-import { GRID_HEIGHT, GRID_WIDTH } from "@constants/gridConstants";
 import { container } from "@providers/inversify/container";
-import { Rectangle } from "@timohausmann/quadtree-ts";
+import { MapSolidsQuadTree } from "../MapSolidsQuadTree";
 import { PathfindingQuadTree } from "../PathfindingQuadTree";
 
 describe("PathfindingQuadtree", () => {
   let pathfindingQuadTree: PathfindingQuadTree;
+  let mapSolidsQuadTree: MapSolidsQuadTree;
 
   beforeAll(() => {
+    mapSolidsQuadTree = container.get(MapSolidsQuadTree);
     pathfindingQuadTree = container.get(PathfindingQuadTree);
   });
 
-  it("generates a grid between 2 points, with proper solids", () => {
-    const grid = pathfindingQuadTree.generateGridBetweenPoints(0, 0, 32, 32, [
-      new Rectangle({
-        x: 5,
-        y: 5,
-        width: GRID_WIDTH,
-        height: GRID_HEIGHT,
-      }),
-      new Rectangle({
-        x: 2,
-        y: 3,
-        width: GRID_WIDTH,
-        height: GRID_HEIGHT,
-      }),
-    ]);
-
-    expect(grid).toBeDefined();
-
-    expect(grid.width).toBe(32);
-    expect(grid.height).toBe(32);
-
-    expect(grid.nodes[5][5].walkable).toBe(false);
-    expect(grid.nodes[5][6].walkable).toBe(true);
-    expect(grid.nodes[5][7].walkable).toBe(true);
-    expect(grid.nodes[2][3].walkable).toBe(false);
+  beforeEach(() => {
+    mapSolidsQuadTree.init("unit-test-map-negative-coordinate", -16, 0, 32, 32);
+    mapSolidsQuadTree.addSolid("unit-test-map-negative-coordinate", -5, 10);
+    mapSolidsQuadTree.addSolid("unit-test-map-negative-coordinate", -10, 10);
+    mapSolidsQuadTree.addSolid("unit-test-map-negative-coordinate", -9, 12);
+    mapSolidsQuadTree.addSolid("unit-test-map-negative-coordinate", -10, 15);
+    mapSolidsQuadTree.addSolid("unit-test-map-negative-coordinate", -7, 16);
   });
 
-  it("calculates a shortest path between points", () => {});
+  it("generates a grid between 2 points, with proper solids and correct width and height", () => {
+    const grid = pathfindingQuadTree.generateGridBetweenPoints("unit-test-map-negative-coordinate", -11, 9, -3, 17);
+
+    expect(grid).toBeDefined();
+    expect(grid.width).toBe(9);
+    expect(grid.height).toBe(9);
+  });
+
+  it("generates a grid with proper width and height", () => {
+    const grid = pathfindingQuadTree.generateGridBetweenPoints("unit-test-map-negative-coordinate", 19, 27, 20, 28);
+
+    expect(grid).toBeDefined();
+    expect(grid.width).toBe(2);
+    expect(grid.height).toBe(2);
+  });
+
+  it("calculates a shortest path between points", () => {
+    const path = pathfindingQuadTree.findShortestPathBetweenPoints(
+      "unit-test-map-negative-coordinate",
+      -11,
+      9,
+      -3,
+      17,
+      16,
+      0
+    );
+
+    console.log(path);
+
+    expect(path).toBeDefined();
+  });
 });
